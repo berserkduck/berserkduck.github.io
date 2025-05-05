b5f2b40766bbda42ec0815cd4351be546fec29c7
ryan <150310208+berserkduck@users.noreply.github.com>
2025-05-05 14:56:25 +0800

    update background colors

--- quartz.config.ts
+++ quartz.config.ts
@@ -48,12 +48,12 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fffcf0",//#faf8f8
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#8fcea1",//#284b63
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
@@ -64,7 +64,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#8fcea1",//#7b97aa
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
